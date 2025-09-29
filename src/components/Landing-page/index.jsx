0dc66d9e48b762bee2d89960e6db9edfe0676c5b
Lewiston <m11lewiston@gmail.com>
2025-09-29 15:31:19 +0100

    Other sections added to the landing page

--- src/components/Landing-page/index.jsx
+++ src/components/Landing-page/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Hero from "./Hero";
+import Sections from "./Sections";
+import "../../styles/hero-sections.css";
 
 const LandingPage = () => {
   return (
     <div>
       <Hero />
+      <Sections />
     </div>
   );
 };
