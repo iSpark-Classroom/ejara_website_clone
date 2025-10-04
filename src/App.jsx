5c655d6702378548c4490b18b7b2dd648d16a6cf
tumenta-favour <tumentafavour6@gmail.com>
2025-10-04 13:02:09 +0100

    chore: add a react router use for routing the navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import "./styles/App.css";
 import LandingPage from "./components/Landing-page";
 import Invest from "./components/Invest";
+import EjaraForBusiness from "./components/Ejara-for-business";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
 function App() {
+    
   return (
-    <div className="main-container">
-      <LandingPage />
-    </div>
+    <BrowserRouter className="main-container">
+   
+    
+
+     <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/invest" element={<Invest />} />
+        <Route path="/business" element={<EjaraForBusiness/>} />
+
+      </Routes>
+
+    </BrowserRouter>
   );
+  
 }
 
 export default App;
---
+++ src/components/Ejara-for-business/Connecting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Connecting() {
+  return (
+    <div>Connecting</div>
+  )
+}
+
+export default Connecting
