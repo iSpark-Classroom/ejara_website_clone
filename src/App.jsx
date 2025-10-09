b5ac3275211150d43a53cedf0bce4ac5f9a5a225
Reece-Sam <bonisamkelekoh@gmail.com>
2025-10-09 17:48:41 +0100

    chores: some changes to my ejara project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./styles/App.css";
 import LandingPage from "./components/Landing-page";
 import Invest from "./components/Invest";
+import Investment from "./components/Invest/Investment";
+import InvestedSafely from "./components/Invest/InvestedSafely";
+import Guaranteed from "./components/Invest/Guaranteed";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <LandingPage />
-    </>
+      <Investment />
+      <InvestedSafely />
+      <Guaranteed />
+    </div>
   );
 }
 
