6a9ece90559a0ddf742bdd7870168419d0d54ae0
BebsYanski <bebongnchu.yannick@isparkinno.com>
2026-03-04 07:55:55 +0100

    Enhance PdfViewer component with pagination controls and improve layout

--- src/components/Terms-and-conditions/index.jsx
+++ src/components/Terms-and-conditions/index.jsx
@@ -5,12 +5,20 @@ import PdfViewer from "./pdf_viewer";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url
+  import.meta.url,
 ).toString();
 
 const TermsAndConditions = () => {
   return (
-    <div>
+    <div
+      style={{
+        padding: "50px 0",
+        marginTop: "50px",
+        backgroundColor: "#F5F5F5",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
       {/* TermsAndConditions <Body /> */}
       <PdfViewer />
     </div>
--- src/components/Terms-and-conditions/pdf_viewer.jsx
+++ src/components/Terms-and-conditions/pdf_viewer.jsx
@@ -1,18 +1,56 @@
-import { Document, Page } from "react-pdf";
-import pdf from "../../assets/CGU_Ejara.pdf";
+import { Document, Page, pdfjs } from "react-pdf";
 import { useState } from "react";
+import pdf from "../../assets/CGU_Ejara.pdf";
+
+// IMPORTANT: Set worker
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
 function PdfViewer() {
-  const [pageNumber, setPageNUmber] = useState(1);
-  function onDocumentLoaded(data) {
-    console.log("Document loaded", data);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [numPages, setNumPages] = useState(null);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    console.log("Total pages:", numPages);
   }
 
   return (
-    <div>
-      <Document file={pdf} onLoadSuccess={onDocumentLoaded}>
-        <Page pageNumber={pageNumber} />
+    <div style={{ textAlign: "center" }}>
+      <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+        {/* <Page pageNumber={pageNumber} /> */}
+        <Page pageNumber={pageNumber} width={window.innerWidth * 0.8} />
       </Document>
+
+      {numPages && (
+        <div
+          style={{
+            marginTop: "20px",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <button
+            disabled={pageNumber <= 1}
+            onClick={() => setPageNumber(pageNumber - 1)}
+          >
+            Previous
+          </button>
+
+          <span style={{ margin: "0 15px" }}>
+            Page {pageNumber} of {numPages}
+          </span>
+
+          <button
+            disabled={pageNumber >= numPages}
+            onClick={() => setPageNumber(pageNumber + 1)}
+          >
+            Next
+          </button>
+        </div>
+      )}
     </div>
   );
 }
+
 export default PdfViewer;
